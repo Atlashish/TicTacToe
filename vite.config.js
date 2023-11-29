2b0027fb0bce5f21487563f9b7189ca0048d5282
Giovanni <giovanni.lor23@gmail.com>
2023-11-29 10:10:47 +0100

    audio verify

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { fileURLToPath, URL } from 'node:url';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
