59edb44605a49b653b0f56fce04d55538c2209cb
Giovanni <giovanni.lor23@gmail.com>
2023-11-29 11:53:31 +0100

    aduio adjust

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   build: {
     assetsInlineLimit: 0,
@@ -27,4 +31,4 @@ export default defineConfig({
       type: 'asset/resource',
     },
   ],
-});
+})
