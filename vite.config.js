0f8e3391ebe045a6e6d8f38eef8a8d011ddf43a8
Giovanni <giovanni.lor23@gmail.com>
2023-11-29 11:48:42 +0100

    aduio adjust

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-  
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    assetsInlineLimit: 0,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['@vueuse/sound'],
+  },
+  rules: [
+    {
+      test: /\.(wav|mp3)$/,
+      type: 'asset/resource',
+    },
+  ],
+});
